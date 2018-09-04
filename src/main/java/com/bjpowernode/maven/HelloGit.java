333da24ff681c27bec50748fab79b8d8a7be3e74
bj <bj@126.com>
2018-09-04 08:53:51 +0800

    009-IDEA整合Git测试和.gitignore忽略文件测试

---
+++ src/main/java/com/bjpowernode/maven/HelloGit.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.maven;
+
+/**
+ * ClassName:HelloGit
+ * Package:com.bjpowernode.maven
+ * Description:
+ *
+ * @Date 2018/9/4 8:41
+ * @Author guoxin
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Git如何提交时忽略相应文件！！！");
+    }
+}
